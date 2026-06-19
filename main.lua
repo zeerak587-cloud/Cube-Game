55560cbc1bd45fead1c742eb8923db47fa7bd80e
Zeerakyoungdev <zeerak587@gmail.com>
2026-06-18 22:31:20 -0400

    Refactor update to draw function for rendering
    Refactor the update function and add a draw function to handle rendering.

--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
 local Player = require("player")
 local Cubes = require("cubes")
-local Rounds = require("rounds")
-local Collision = require("collision")
 
-function lovr.update(dt)
-    Player.update(dt)
-    Cubes.update(dt)
-    Rounds.update(dt)
+function lovr.draw(pass)
+    -- Floor
+    pass:setColor(.3, .8, .3)
+    pass:box(0, -0.5, 0, 50, 1, 50)
+
+    -- Player
+    pass:setColor(0, 0, 1)
+    pass:box(Player.x, Player.y, Player.z, 1, 2, 1)
+
+    -- Falling cubes
+    pass:setColor(1, 0, 0)
 
     for _, cube in ipairs(Cubes.list) do
-        if Collision.playerCube(Player, cube) then
-            Player.reset()
-            Cubes.clear()
-            Rounds.reset()
-            break
-        end
+        pass:box(cube.x, cube.y, cube.z, 1, 1, 1)
     end
 end
